dc5c97edccb4a3f03b5a9191680f679a7f233cc0
Sydney Erickson <sydneyerickson@icloud.com>
2017-08-29 12:53:10 -0700

    This should work!

--- plugins/search/__init__.py
+++ plugins/search/__init__.py
@@ -1,5 +1,25 @@
-from api import command, plugin
+from api import command, plugin, message
+import google
+import urllib
 
+# Search plugin
 def onInit(plugin_in):
-    wikipedia_command = command.Command(plugin_in, 'wikipedia', shortdesc='Search Wikipedia, The Free Encyclopedia')
-    return plugin.Plugin(plugin_in, 'wikipedia', [wikipedia_command])
+    google_command = command.Command(plugin_in, 'google', shortdesc='Google it!')
+    bing_command = command.Command(plugin_in, 'bing', shortdesc='Uhh... Bing it?')
+    duckduckgo_command = command.Command(plugin_in, 'duckduckgo', shortdesc='Ask the duck.')
+    aol_command = command.Command(plugin_in, 'aol', shortdesc='Here you go, gramps')
+    return plugin.Plugin(plugin_in, 'search', [google_command, bing_command, duckduckgo_command, aol_command])
+
+async def onCommand(message_in):
+    query = message_in.body.strip()
+
+    # Check if query is nothing
+    if not query:
+        return message.Message('I need a topic to search for!')
+
+    # Normalize query
+    query = urllib.parse.quote(query)
+
+    # Be kind, don't use lmgtfy/similar
+    if message_in.command == 'google':
+        return message.Message(google.search(query))
---
+++ plugins/search/google.py
@@ -0,0 +1,4 @@
+def search(term):
+    msg = "Google search:"
+    url = "https://www.google.com/#q="
+    return '{}{}'.format(msg, url)
