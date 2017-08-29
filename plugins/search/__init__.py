67ea7e9737f76aaa9557a9f5b4ee8f43c09e5da4
Sydney Erickson <sydneyerickson@icloud.com>
2017-08-29 12:49:40 -0700

    Forgot to import stuff for new search plugin

--- plugins/search/__init__.py
+++ plugins/search/__init__.py
@@ -1,3 +1,5 @@
+from api import command, plugin
+
 def onInit(plugin_in):
     wikipedia_command = command.Command(plugin_in, 'wikipedia', shortdesc='Search Wikipedia, The Free Encyclopedia')
     return plugin.Plugin(plugin_in, 'wikipedia', [wikipedia_command])
